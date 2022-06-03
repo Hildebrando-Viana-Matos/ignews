daeeba25d3d36b3009f179590248a78624ab2c94
Hildebrando Viana Matos <hildocontato@gmail.com>
2022-06-03 10:07:06 -0300

    Using SSR and SSG on Next, and using Stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
+import { GetStaticProps } from "next";
+
 import Head from "next/head";
 
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
 import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -19,9 +29,9 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9,90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl Coding" />
@@ -29,3 +39,26 @@ export default function Home() {
     </>
   );
 }
+
+// getServerSideProps make request on NextJS Node Server and not in Browser every time that the user it's on page, and it show the data before the Browser reload.
+// getStaticProps make request on NextJS Node Server, but it save a static HTML, and show that HTML for ALL USERS, because the content don't change, and we set a revalidate to warning when that data have to be revalidate for show a new Static HTML.
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1L6ZZQGYU48moCypGnZOQXc7", {
+    expand: ["product"],
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
