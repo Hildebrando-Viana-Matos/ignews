8d3ffc7c86c6ccb73221a114570ca2077f700aa0
Hildebrando Viana Matos <hildocontato@gmail.com>
2022-06-25 17:07:27 -0300

    Listing all posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,13 +1,24 @@
 import { GetStaticProps } from "next";
 import Head from "next/head";
 
-import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
 
 import { getPrismicClient } from "../../services/prismic";
 
 import styles from "./styles.module.scss";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -16,30 +27,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="">
-            <time>12 de julho de 2022</time>
-            <strong>Next Level Week: os caminhos que trilhamos até aqui</strong>
-            <p>
-              Conheça um pouco mais de tudo que já foi feito na história de um
-              dos maiores eventos online de programação do Brasil
-            </p>
-          </a>
-          <a href="">
-            <time>12 de julho de 2022</time>
-            <strong>Next Level Week: os caminhos que trilhamos até aqui</strong>
-            <p>
-              Conheça um pouco mais de tudo que já foi feito na história de um
-              dos maiores eventos online de programação do Brasil
-            </p>
-          </a>
-          <a href="">
-            <time>12 de julho de 2022</time>
-            <strong>Next Level Week: os caminhos que trilhamos até aqui</strong>
-            <p>
-              Conheça um pouco mais de tudo que já foi feito na história de um
-              dos maiores eventos online de programação do Brasil
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug} href="">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -53,9 +47,25 @@ export const getStaticProps: GetStaticProps = async () => {
     lang: "pt-BR",
   });
 
-  console.log(response);
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
 
   return {
-    props: {},
+    props: { posts },
   };
 };
