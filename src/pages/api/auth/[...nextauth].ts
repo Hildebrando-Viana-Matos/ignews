fdd2d2e810cc5b040d07adfed1a572504908bef9
Hildebrando Viana Matos <hildocontato@gmail.com>
2022-06-26 14:54:24 -0300

    Making preview post

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,8 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 
 import { query as q } from "faunadb";
-import { fauna } from './../../../services/fauna';
+import { fauna } from "./../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -13,42 +13,63 @@ export default NextAuth({
       authorization: {
         params: {
           // Telling what information we need from users
-          scope: 'read:user',
-        }
-      }
+          scope: "read:user",
+        },
+      },
     }),
   ],
   callbacks: {
-    async signIn({user, account, profile}) {
-        const email = user.email;
-
-        try {
-          await fauna.query(
-            q.If(
-              q.Not(
-                q.Exists(
-                  q.Match(
-                    q.Index("user_by_email"),
-                    q.Casefold(user.email)
+    async session({ session }) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index("subscription_by_user_ref"),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index("user_by_email"),
+                      q.Casefold(session.user.email)
+                    )
                   )
                 )
               ),
-              q.Create(
-                q.Collection("users"),
-                { data: {email} }
-              ),
-              q.Get(
-                q.Match(
-                  q.Index("user_by_email"),
-                  q.Casefold(user.email)
-                )
-              )
-            )
+              q.Match(q.Index("subscription_by_status"), "active"),
+            ])
           )
-          return true;
-        } catch (error) {
-        return false; 
-        }
-    }
-  }
-})
+        );
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription,
+        };
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        };
+      }
+    },
+    async signIn({ user, account, profile }) {
+      const email = user.email;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+  },
+});
